38ed3db28c38d399fa1ed3533a896d8c8cd534ed
AlexEvk <recnoi88@rambler.ru>
2022-06-06 17:14:27 +0300

    исправила хибер

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -8,139 +8,99 @@ import org.hibernate.Transaction;
 import org.hibernate.query.Query;
 
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
     public UserDaoHibernateImpl() {
-
     }
 
-    @Override
-    public void createUsersTable() {
-        Session session = Util.getSession();
+    public void openCloseSession(String SQL){
         Transaction transaction = null;
-        try {
+        try (Session session = Util.getSession()){
             transaction = session.beginTransaction();
-
-            String SQL = "CREATE TABLE IF NOT EXISTS users(Id SERIAL PRIMARY KEY, "
-                    + "USER_NAME VARCHAR(200) NOT NULL, "
-                    + "LAST_NAME VARCHAR(200) NOT NULL, "
-                    + "AGE SMALLINT NOT NULL)";
-
             Query query = session.createNativeQuery(SQL);
             query.executeUpdate();
             transaction.commit();
         }
         catch (HibernateException e) {
-            transaction.rollback();
+            if(transaction != null){
+                transaction.rollback();
+            }
             e.printStackTrace();
         }
-        finally {
-            session.close();
-        }
+    }
+    @Override
+    public void createUsersTable() {
+        String SQL = "CREATE TABLE IF NOT EXISTS users(Id SERIAL PRIMARY KEY, "
+                    + "USER_NAME VARCHAR(200) NOT NULL, "
+                    + "LAST_NAME VARCHAR(200) NOT NULL, "
+                    + "AGE SMALLINT NOT NULL)";
+        openCloseSession(SQL);
     }
 
     @Override
     public void dropUsersTable() {
-        Session session = Util.getSession();
-        Transaction transaction = null;
-        try {
-            transaction = session.beginTransaction();
-
-            String SQL = "DROP TABLE IF EXISTS users";
-
-            Query query = session.createNativeQuery(SQL);
-            query.executeUpdate();
-            transaction.commit();
-        }
-        catch (HibernateException e) {
-            transaction.rollback();
-            e.printStackTrace();
-        }
-        finally {
-            session.close();
-        }
+        String SQL = "DROP TABLE IF EXISTS users";
+        openCloseSession(SQL);
     }
 
     @Override
     public void saveUser(User user) {
-        Session session = Util.getSession();
         Transaction transaction = null;
-        try {
+        try (Session session = Util.getSession()){
             transaction = session.beginTransaction();
             session.save(user);
             transaction.commit();
         }
         catch (HibernateException e) {
-            transaction.rollback();
+            if(transaction != null){
+                transaction.rollback();
+            }
             e.printStackTrace();
-        }
-        finally {
-            session.close();
         }
     }
 
     @Override
     public void removeUserById(long id) {
-        Session session = Util.getSession();
         Transaction transaction = null;
-        try {
+        try (Session session = Util.getSession()){
             transaction = session.beginTransaction();
-
-            User user = session.get(User.class, id);
-            if (user != null) {
-                session.delete(user);
-            }
+            String SQL = "DELETE FROM User WHERE id = :id";
+            session.createNativeQuery(SQL)
+                    .setParameter("id", id)
+                    .executeUpdate();
             transaction.commit();
         }
         catch (HibernateException e) {
-            transaction.rollback();
+            if(transaction != null){
+                transaction.rollback();
+            }
             e.printStackTrace();
-        }
-        finally {
-            session.close();
         }
     }
 
     @Override
     public List<User> getAllUsers() {
-        Session session = Util.getSession();
         Transaction transaction = null;
-        try {
+        List<User> users = new ArrayList<>();
+        try (Session session = Util.getSession()){
             transaction = session.beginTransaction();
-            List<User> users = session.createNativeQuery("SELECT*FROM users",User.class).list();
+            users = session.createNativeQuery("SELECT*FROM users",User.class).list();
             transaction.commit();
-            return users;
         }
         catch (HibernateException e) {
-            transaction.rollback();
+            if(transaction != null){
+                transaction.rollback();
+            }
             e.printStackTrace();
         }
-        finally {
-            session.close();
-        }
-        return null;
+        return users;
     }
 
     @Override
     public void cleanUsersTable() {
-        Session session = Util.getSession();
-        Transaction transaction = null;
-        try {
-            transaction = session.beginTransaction();
-
-            String SQL = "TRUNCATE TABLE users ";
-
-            Query query = session.createNativeQuery(SQL);
-            query.executeUpdate();
-            transaction.commit();
-        }
-        catch (HibernateException e) {
-            transaction.rollback();
-            e.printStackTrace();
-        }
-        finally {
-            session.close();
-        }
+        String SQL = "TRUNCATE TABLE users ";
+        openCloseSession(SQL);
     }
 }
